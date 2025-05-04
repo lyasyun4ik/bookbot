19f2d238632b84f9c5042ac180c76cc37b60c19d
lyasyun4ik <uliana.zaspa@hotmail.com>
2025-05-04 19:50:22 +0200

    use sys.argv to make it work with any book entered through terminal

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import sys
 from stats import get_num_words, count_char, sort_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book = get_book_text(book_path)
     num_words = get_num_words(book)
     char_dict = count_char(book)
     char_dict_list = sort_dict(char_dict)
 
     print(f"============ BOOKBOT ============ \n"
-f"Analyzing book found at {book_path}...\n"
-f"----------- Word Count ----------\n"
-f"Found {num_words} total words\n"
-f"--------- Character Count -------")
+    f"Analyzing book found at {book_path}...\n"
+    f"----------- Word Count ----------\n"
+    f"Found {num_words} total words\n"
+    f"--------- Character Count -------")
     for item in char_dict_list:
         if item["char"].isalpha():
             print(f"{item["char"]}: {item["num"]}")
